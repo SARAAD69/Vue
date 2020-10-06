e7da8984d174ea9801e5dac865168cc725c801f6
Artemii Chernega <a.chernega2000@gmail.com>
2020-10-06 17:44:46 +0300

    Webpack and Vue CLI setup - 416

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1><span>Hello</span> World!</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="sass">
+  h1
+    color: red
+
+    span
+      color: blue
+</style>
