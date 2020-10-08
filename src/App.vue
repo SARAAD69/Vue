9566ba5055b9f68cf1cedbd0d650ae523c64d35e
Artemii Chernega <a.chernega2000@gmail.com>
2020-10-08 18:03:30 +0300

    An Introduction to Components [VUE2] - 425

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-    <h1><span>Hello</span> World!</h1>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-export default {
-  
-}
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
 </script>
 
-<style lang="sass">
-  h1
-    color: red
+<style lang='sass'>
 
-    span
-      color: blue
 </style>
---
+++ src/LebabTest.js
@@ -0,0 +1,5 @@
+var x = function(){
+    return {
+        status: 'Critical'
+    } 
+  }
